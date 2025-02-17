864a35aba7f2d1f17e56c0956d8d9de149c3321e
Khairulla-FrontEnd <khairulla2008@outlook.com>
2025-02-17 21:20:14 +0500

    fix production

--- frontend/environments/environment.js
+++ frontend/environments/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl:'http://localhost:3500',
+    apiUrl:'https://task-manager-us3r.onrender.com',
     apiKey:"af27936b26c23be040dbe5da9d142f23ec84c0ecb7174ab376b3b178b7f90b24"
 };
